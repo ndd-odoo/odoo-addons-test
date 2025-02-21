90442020fd23ed0e58d429db9d5fd02412c5bc6a
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2025-02-21 09:49:31 +0700

    preview viin_bank_connector

--- student_attendance_management/__manifest__.py
+++ student_attendance_management/__manifest__.py
@@ -1,29 +1,14 @@
 {
-    "name": "Student Attendance Management",
+    "name": "Test index html",
     "version": "1.0",
-    'website': "https://altaif.com.sg/",
-    'author': "Altaif Singapore PTE. LTD.",
+    'website': "https://example.com.sg/",
+    'author': "ddd",
     "Category": "HR",
-    "Summary": "Manages Attendance for students using the HR Attendance Module",
+    "Summary": "Test index",
     "depends": ['base', 'hr', 'hr_attendance', 'website_slides'],
     "data": [
-        'security/ir.model.access.csv',
-        'wizards/print_student_badge_views.xml',
-        'reports/res_partner_badge.xml',
-        'views/hr_attendance_views.xml',
-        'views/hr_employee_views.xml',
-        'views/res_partner_views.xml'
     ],
     'assets': {
-        # 'web.assets_backend': [
-        #     'student_attendance_management/static/src/**/*',
-        # ],
-        # 'web.assets_frontend': [
-        #     'student_attendance_management/static/src/**/*',
-        # ],
-        'hr_attendance.assets_public_attendance': [
-            'student_attendance_management/static/src/public_kiosk/**/*',
-        ],
     },
     'installable': True,
     'auto_install': False,
--- student_attendance_management/models/hr_attendance.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class HrAttendance(models.Model):
-    _inherit = 'hr.attendance'
-    _description = 'Hr Attendance'
-
-    slide_channel_id = fields.Many2one('slide.channel', 'Course')
